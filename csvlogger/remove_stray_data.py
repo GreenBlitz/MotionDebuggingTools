187ccc984f55aaf68ecdfde56c08425b1357555a
GBSoftwareLaptop <you@example.com>
2021-03-25 20:47:22 +0200

    Added feature for multi-file error filtering

--- csvlogger/remove_stray_data.py
+++ csvlogger/remove_stray_data.py
@@ -3,20 +3,26 @@ from numbers import Real
 
 def main():
     assert len(sys.argv) > 2
-    delete_error = sys.argv[2]
+    delete_error = sys.argv[-1]
     assert delete_error.isnumeric()
+
+    for i in sys.argv[1:-1]:
+        remove_stray(i)
+
+def remove_stray(filename):
     filename = sys.argv[1]
 
 
     lines = open(filename, "r").read().split("\n")
-    print(lines)
     with open(filename, "w") as f:
         l = ""
-        for line in lines[1:]:
-            print(line)
+        for line in lines[1:-1]:
             a = line.split(",")
-            if abs(float(a[-1])) < float(delete_error) and line != "\n":
-                l += "\n" + line
+            try:
+                if abs(float(a[-1])) < float(delete_error) and line != "\n":
+                    l += "\n" + line
+            except:
+                print(line)
 
         f.write(lines[0] + l)
 
